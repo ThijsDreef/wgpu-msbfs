6eb8658b3fa7d592dd21bb1c0b6aa229686fb6b4
Thijs Dreef <thijs9907@gmail.com>
2025-03-18 13:52:36 +0100

    Fix to work with updated wgpu API

--- src/wgpu-state.cpp
+++ src/wgpu-state.cpp
@@ -5,31 +5,39 @@
 #include "wgpu-state.hpp"
 
 
-void error(WGPUErrorType type, const char *msg, void *user) {
+void error(WGPUDeviceImpl *const * x, WGPUErrorType type, WGPUStringView msg, void *user, void* s) {
   std::cout << "[WGPU Error] " << type << " ";
-  if (msg)
-    std::cout << msg;
+  if (msg.data)
+    std::cout << msg.data;
   std::cout << std::endl;
 }
 
 WGPUState::WGPUState() {
   instance = wgpu::createInstance({});
   wgpu::RequestAdapterOptions options;
-  wgpu::Adapter adapter = instance.requestAdapter({});
+  options.powerPreference = wgpu::PowerPreference::LowPower;
+  wgpu::Adapter adapter = instance.requestAdapter(options);
 
   wgpu::FeatureName required_features[] = {
     wgpu::FeatureName::TimestampQuery,
   };
 
   wgpu::DeviceDescriptor device_desc;
+  device_desc.uncapturedErrorCallbackInfo.callback = error;
   device_desc.requiredFeatures =
     reinterpret_cast<WGPUFeatureName *>(required_features);
   device_desc.requiredFeatureCount = 1;
 
-#ifdef WEBGPU_BACKEND_DAWN
-  device_desc.uncapturedErrorCallbackInfo.callback = error;
-  device_desc.uncapturedErrorCallbackInfo.userdata = nullptr;
-#endif
+  wgpu::Limits limits;
+  limits.setDefault();
+  limits.maxStorageBuffersPerShaderStage = 7;
+
+  wgpu::AdapterInfo info;
+  adapter.getInfo(&info);
+  std::cout << info.vendor.data << std::endl;
+  std::cout << info.device.data << std::endl;
+  device_desc.requiredLimits = &limits;
+
   device = adapter.requestDevice(device_desc);
   queue = device.getQueue();
   expand = std::unique_ptr<FrontierExpansion>(new FrontierExpansion(device));
