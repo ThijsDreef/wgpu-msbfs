2424d2129d818083e5071d5f2210adc0c87b4b54
Thijs Dreef <thijs9907@gmail.com>
2025-05-07 16:15:51 +0200

    interleaved Msbfs Cuda

--- src/cuda/msbfs.cu
+++ src/cuda/msbfs.cu
@@ -9,16 +9,18 @@
 #include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
 
+#define to_interleaved(var) (var * blockDim.x) + threadIdx.x
+
 struct SearchInfo {
   uint32_t iteration;
   uint32_t mask;
   uint32_t jfq_length;
 };
 
-__global__ void set_first_bsak(uint32_t *bsak, uint32_t* src, uint32_t v_length) {
-  for (uint32_t i = threadIdx.x; i < 32; i += blockDim.x) {
-    uint32_t offset = (blockIdx.x * v_length);
-    atomicOr(bsak + offset + src[i + blockIdx.x * 32], 1u << i);
+__global__ void set_first_bsak(uint32_t *bsak, uint32_t* src, uint32_t total_length) {
+  for (uint32_t i = threadIdx.x; i < 32 && (i + blockIdx.x * 32) < total_length; i += 1) {
+    uint32_t v_loc = src[i + (blockIdx.x * 32)] * gridDim.x + blockIdx.x;
+    atomicOr(bsak + v_loc, 1u << i);
   }
 }
 
@@ -26,48 +28,48 @@ __global__ void identify_step(uint32_t v_length, SearchInfo *info,
                               uint32_t *jfq, uint32_t *dst,
                               uint32_t *path_length, uint32_t *bsa,
                               uint32_t *bsak) {
-  uint32_t v_offset = v_length * blockIdx.x;
-  uint32_t c_mask = ~(info[blockIdx.x].mask);
-  uint32_t iteration = info[blockIdx.x].iteration;
-  if (info[blockIdx.x].jfq_length == 0 && info[blockIdx.x].iteration > 0) {
+
+  const uint32_t id = threadIdx.x;
+  uint32_t c_mask = ~(info[id].mask);
+  uint32_t iteration = info[id].iteration;
+  if (info[id].jfq_length == 0 && info[id].iteration > 0) {
     return;
   }
-  info[blockIdx.x].jfq_length = 0;
+  info[id].jfq_length = 0;
   __syncthreads();
 
-  for (uint32_t i = threadIdx.x + blockIdx.y * blockDim.x; i < v_length; i += blockDim.x * gridDim.y) {
-    uint32_t diff = (bsa[v_offset + i] ^ bsak[v_offset + i]) & c_mask;
+  for (uint32_t i = blockIdx.x; i < v_length; i += gridDim.x) {
+    uint32_t diff = (bsa[to_interleaved(i)] ^ bsak[to_interleaved(i)]) & c_mask;
     if (diff == 0) continue;
 
-    bsak[v_offset + i] |= bsa[v_offset + i];
-    uint32_t id = atomicAdd(&info[blockIdx.x].jfq_length, 1u);
-    jfq[v_offset + id] = i;
+    bsak[to_interleaved(i)] |= bsa[to_interleaved(i)];
+    jfq[to_interleaved(atomicAdd(&info[id].jfq_length, 1u))] = i;
 
     uint32_t length = __popc(diff);
     for (uint32_t x = 0; x < length; x++) {
       uint32_t index = 31 - __clz(diff);
-      if (dst[index + blockIdx.x * 32] == i) {
-        path_length[index + blockIdx.x * 32] = iteration;
+      if (dst[index + id * 32] == i) {
+        path_length[index + id * 32] = iteration;
         c_mask &= ~(1 << index);
       }
       diff &= ~(1u << index);
     }
   }
-  atomicOr(&info[blockIdx.x].mask, ~c_mask);
-  info[blockIdx.x].iteration = iteration + 1;
+  atomicOr(&info[id].mask, ~c_mask);
+  info[id].iteration = iteration + 1;
 }
+
 __global__ void expand_step(uint32_t v_length, uint32_t* v, uint32_t* e, SearchInfo *info, uint32_t *jfq,
                             uint32_t *bsa, uint32_t *bsak) {
-  const uint32_t v_offset = v_length * blockIdx.x;
-  const uint32_t length = info[blockIdx.x].jfq_length;
-  for (uint32_t i = threadIdx.x + blockIdx.y * blockDim.x; i < length; i += blockDim.x * gridDim.y) {
-    const uint32_t source = jfq[v_offset + i];
-    const uint32_t val = bsa[v_offset + source];
+  const uint32_t length = info[threadIdx.x].jfq_length;
+  for (uint32_t i = blockIdx.x; i < length; i += gridDim.x) {
+    const uint32_t source = jfq[to_interleaved(i)];
+    const uint32_t val = bsa[to_interleaved(source)];
 
     uint32_t start = v[source] + threadIdx.y;
     const uint32_t end = v[source + 1];
     for (; start < end; start += blockDim.y) {
-      atomicOr(bsak + v_offset + e[start], val);
+      atomicOr(bsak + (to_interleaved(e[start])), val);
     }
   }
 }
@@ -81,7 +83,7 @@ std::vector<IterativeLengthResult> iterative_length(PathFindingRequest request,
 std::vector<IterativeLengthResult> iterative_length(PathFindingRequest request,
                                                     CSR csr, TimingInfo &info) {
   cudaSetDevice(0);
-  const size_t WORKGROUPS = 1;
+  const size_t WORKGROUPS = 8;
   const size_t SEARCHES_IN_WORKGROUP = 32;
   const size_t PAIRS_IN_PARALLEL = WORKGROUPS * SEARCHES_IN_WORKGROUP;
 
@@ -115,6 +117,8 @@ std::vector<IterativeLengthResult> iterative_length(PathFindingRequest request,
   cudaMemcpy(src, request.src, request.length * sizeof(uint32_t), cudaMemcpyHostToDevice);
   cudaMemcpy(dst, request.dst, request.length * sizeof(uint32_t), cudaMemcpyHostToDevice);
 
+  uint32_t* bsak_temp = new uint32_t[csr.v_length * WORKGROUPS];
+
   CSR cuda_csr = csr;
   cuda_csr.v = v_buffer;
   cuda_csr.e = e_buffer;
@@ -125,28 +129,29 @@ std::vector<IterativeLengthResult> iterative_length(PathFindingRequest request,
     cudaMemset(bsak, 0, v_size * WORKGROUPS);
     cudaMemset(search_info, 0, sizeof(SearchInfo) * WORKGROUPS);
     // Setup BSAK
-    set_first_bsak<<<WORKGROUPS, 32>>>(bsak, src + offset, csr.v_length);
-    dim3 grid(WORKGROUPS, 92 / WORKGROUPS, 1);
-    dim3 block(128, 8, 1);
+    set_first_bsak<<<WORKGROUPS, 32>>>(bsak, src + offset, request.length - offset);
+//    cudaDeviceSynchronize();
+    dim3 grid(92 * 4, 1, 1);
+    dim3 block(WORKGROUPS, 4, 1);
     uint32_t jfq_lengths = 1;
     for (int iteration = 0; jfq_lengths > 0; iteration++) {
       if (iteration % 2 == 1) {
-        identify_step<<<grid, 256>>>(csr.v_length, search_info, jfq, dst + offset, path_lengths + offset, bsa, bsak);
+        identify_step<<<grid, WORKGROUPS>>>(csr.v_length, search_info, jfq, dst + offset, path_lengths + offset, bsa, bsak);
         cudaDeviceSynchronize();
         expand_step<<<grid, block>>>(cuda_csr.v_length, v_buffer, e_buffer, search_info, jfq, bsa, bsak);
       } else {
-        identify_step<<<grid, 256>>>(csr.v_length, search_info, jfq, dst + offset, path_lengths + offset, bsak, bsa);
+        identify_step<<<grid, WORKGROUPS>>>(csr.v_length, search_info, jfq, dst + offset, path_lengths + offset, bsak, bsa);
         cudaDeviceSynchronize();
         expand_step<<<grid, block>>>(cuda_csr.v_length, v_buffer, e_buffer, search_info, jfq, bsak, bsa);
       }
       cudaDeviceSynchronize();
+
       if (iteration % 10 == 0) {
         cudaMemcpy(debug, search_info, WORKGROUPS * sizeof(SearchInfo), cudaMemcpyDeviceToHost);
         jfq_lengths = 0;
         for (size_t i = 0; i < WORKGROUPS; i++) {
           jfq_lengths += debug[2 + 3 * i];
         }
-//        std::cout << jfq_lengths << std::endl;
       }
 
      }
@@ -162,7 +167,6 @@ std::vector<IterativeLengthResult> iterative_length(PathFindingRequest request,
       .length = host_result[j],
     });
   }
-
   cudaFree(v_buffer);
   cudaFree(e_buffer);
 
