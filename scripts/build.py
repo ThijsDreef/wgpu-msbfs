6afc6183f3ce2d86512af803533adaa55be23af7
Thijs Dreef <thijs9907@gmail.com>
2025-06-13 20:38:47 +0200

    add build script + bench larger datasets

---
+++ scripts/build.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+
+def get_output_name(prefix):
+    return f'{prefix}_coalesced-access.json'
+
+cuda = {
+    "create_build_directory": ["cmake", "-S", ".", "-B", "build/cuda", "-DBACKEND=CUDA"],
+    "build": ["cmake", "--build", "build/cuda"],
+    "test": ["./build/cuda/tests/test_wgpumsbfs"],
+    "benchmark": ["./build/cuda/benchmarks/bench_wgpumsbfs", f'--benchmark_out={get_output_name('cuda')}']
+}
+
+wgpu = {
+    "create_build_directory": ["cmake", "-S", ".", "-B", "build/wgpu", "-DBACKEND=WGPU", "-DWEBGPU_BUILD_FROM_SOURCE=OFF"],
+    "build": ["cmake", "--build", "build/wgpu"],
+    "test": ["./build/wgpu/tests/test_wgpumsbfs"],
+    "benchmark": ["./build/wgpu/benchmarks/bench_wgpumsbfs", f'--benchmark_out={get_output_name('wgpu')}']
+}
+
+dawn = {
+    "create_build_directory": ["cmake", "-S", ".", "-B", "build/dawn", "-DBACKEND=DAWN", "-DWEBGPU_BUILD_FROM_SOURCE=OFF"],
+    "build": ["cmake", "--build", "build/dawn"],
+    "test": ["./build/dawn/tests/test_wgpumsbfs"],
+    "benchmark": ["./build/dawn/benchmarks/bench_wgpumsbfs", f'--benchmark_out={get_output_name('dawn')}']
+}
+
+
+targets = [
+    wgpu,
+    dawn
+]
+
+try:
+    subprocess.check_output('nvidia-smi')
+    targets.append(cuda)
+except Exception: # this command not being found can raise quite a few different errors depending on the configuration
+    pass
+
+workgroups = [1, 2, 4]
+
+subprocess.run(["python", "scripts/generate-tests.py"])
+for target in targets:
+    for prop in target:
+        subprocess.run(target[prop]).check_returncode()
